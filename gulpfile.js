b42c72bde5b47a7758369c48084a68cd629ba159
Deku <gurycvadym510@gmail.com>
2021-07-20 11:57:53 +0300

    Add fonts, Setting gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const sass = require("gulp-sass")(require("sass"));
 const sourcemaps = require("gulp-sourcemaps");
 const minify = require("gulp-minify-css");
 const concat = require("gulp-concat");
+const htmlmin = require("gulp-htmlmin");
 
 gulp.task("styles", () => {
   return gulp
@@ -12,13 +13,28 @@ gulp.task("styles", () => {
     .pipe(sourcemaps.write("."))
     .pipe(gulp.dest("./style/css/"));
 });
+gulp.task("minify-html", function () {
+  return gulp
+    .src("./src/index.html") // указываем пути к файлам .html
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true, // удаляем все переносы
+        removeComments: true, // удаляем все комментарии
+      })
+    )
+    .pipe(gulp.dest("./")); // оптимизированные файлы .html переносим на продакшен
+});
 gulp.task("minify", function () {
   gulp.src("css/*.css").pipe(minify()).pipe(gulp.dest("./css/"));
 });
 gulp.task("watch", () => {
   gulp.watch("style/scss/**/*.scss", (done) => {
-    gulp.series(["styles", "minify"])(done);
+    gulp.series(["styles", "minify", "minify-html"])(done);
+  });
+  gulp.watch("./src/index.html", (done) => {
+    gulp.series(["minify-html"])(done);
   });
 });
+
 gulp.task("default", gulp.series(["styles", "minify"]));
 ``;
